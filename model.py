85cc670b61ce0a9fd27951a9ff1746250b1876d1
Khamar-Uz-Zama <khamarzama007@gmail.com>
2020-06-15 02:24:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -10,14 +10,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
+from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
-
-
-
 
 data = pd.read_csv('data.csv', index_col=False)
 data = data.drop(data.columns[0],1)
@@ -25,38 +22,34 @@ X = data.iloc[:,:-1]
 labels = data.iloc[:,-1]
 y = labels
 
-
 labelencoder = LabelEncoder()
 y = labelencoder.fit_transform(y)
-
+num_classes = len(labels.unique())
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=0)
-#
-#regressor = LinearRegression()
-#regressor.fit(X_train,Y_train)
-#y_preds = regressor.predict(X_test)
 
-
-dt = DecisionTreeClassifier()
-
-
-dt.fit(X_train, y_train)
-y_pred = dt.predict(X_test)
-
-
-cf_matrix = confusion_matrix(y_test, y_pred)
-
-
-#sns.heatmap(cf_matrix, annot=True)
-
-sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
-            fmt='.2%', cmap='Blues')
-plt.figure()
-plt.ylabel('True label')
-plt.xlabel('Predicted label')
+import keras
+from keras.models import Sequential,Input,Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
 
 
 
-
+cnnModel = Sequential()
+cnnModel.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+cnnModel.add(LeakyReLU(alpha=0.1))
+cnnModel.add(MaxPooling2D((2, 2),padding='same'))
+cnnModel.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+cnnModel.add(LeakyReLU(alpha=0.1))
+cnnModel.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+cnnModel.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+cnnModel.add(LeakyReLU(alpha=0.1))                  
+cnnModel.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+cnnModel.add(Flatten())
+cnnModel.add(Dense(128, activation='linear'))
+cnnModel.add(LeakyReLU(alpha=0.1))                  
+cnnModel.add(Dense(num_classes, activation='softmax'))
 
 
 
@@ -88,8 +81,24 @@ plt.xlabel('Predicted label')
 
 
 
+def decisionTree():
 
-
+    dt = DecisionTreeClassifier()
+    dt.fit(X_train, y_train)
+    y_pred = dt.predict(X_test)
+    cf_matrix = metrics.confusion_matrix(y_test, y_pred)
+    
+    #sns.heatmap(cf_matrix, annot=True)
+    # Plot percentage of data
+    
+    sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
+                fmt='.2%', cmap='Blues')
+    plt.figure()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+    accuracy = metrics.accuracy_score(y_test,y_pred)
+    print("Accuracy using Decision Tree:" ,accuracy)
 
 def plotGenres():
     targetCounts = labels.value_counts()
